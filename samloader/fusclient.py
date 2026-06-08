d4770531abdb2a2f5d4d19148bb82826e2687d5f
AnanJaser <ananjaser@gmail.com>
2026-06-08 12:57:10 +0400

    Fix client request params
    Co-Authored-By: henr1kas <161259379+henr1kas@users.noreply.github.com>

--- samloader/fusclient.py
+++ samloader/fusclient.py
@@ -12,15 +12,14 @@ import xml.dom.minidom
 class FUSClient:
     """ FUS API client. """
     def __init__(self):
+        self.session = requests.Session()
         self.auth = ""
-        self.sessid = ""
         self.makereq("NF_DownloadGenerateNonce.do") # initialize nonce
     def makereq(self, path: str, data: str = "") -> str:
         """ Make a FUS request to a given endpoint. """
         authv = 'FUS nonce="", signature="' + self.auth + '", nc="", type="", realm="", newauth="1"'
-        req = requests.post("https://neofussvr.sslcs.cdngc.net/" + path, data=data,
-                            headers={"Authorization": authv, "User-Agent": "Kies2.0_FUS"},
-                            cookies={"JSESSIONID": self.sessid})
+        req = self.session.post("https://neofussvr.sslcs.cdngc.net/" + path, data=data,
+                            headers={"Authorization": authv, "User-Agent": "Kies2.0_FUS", "X-FUS-Protocol-Ver": "2.0"})
         log_response(f"Request from FUSClient_MakeReq : \nAuthorization : {authv}\n Path : {path}\n Data Byte: {data}\n FinalRequest : {req.request.url} \n Headers : \n {req.request.headers}")
         # If a new NONCE is present, decrypt it and update our auth token.
         log_response(f"Nonce Request from {path}:\n{req.text}")
@@ -28,9 +27,6 @@ class FUSClient:
             self.encnonce = req.headers["NONCE"]
             self.nonce = auth.decryptnonce(self.encnonce)
             self.auth = auth.getauth(self.nonce)
-        # Update the session cookie if needed.
-        if "JSESSIONID" in req.cookies:
-            self.sessid = req.cookies["JSESSIONID"]
         req.raise_for_status()
         return req.text
     def downloadfile(self, filename: str, start: int = 0) -> requests.Response:
@@ -38,7 +34,7 @@ class FUSClient:
         # In a cloud request, we also need to pass the server nonce.
         authv = 'FUS nonce="' + self.encnonce + '", signature="' + self.auth \
             + '", nc="", type="", realm="", newauth="1"'
-        headers = {"Authorization": authv, "User-Agent": "Kies2.0_FUS"}
+        headers = {"Authorization": authv, "User-Agent": "Kies2.0_FUS", "X-FUS-Protocol-Ver": "2.0"}
         if start > 0:
             headers["Range"] = "bytes={}-".format(start)
         req = requests.get("http://cloud-neofussvr.samsungmobile.com/NF_DownloadBinaryForMass.do",
